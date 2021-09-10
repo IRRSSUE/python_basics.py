ddfa13450711738e2559a73a4ac2262d46bc4162
Austin Rolert <73430185+IRRSSUE@users.noreply.github.com>
2021-09-10 06:46:49 +0630

    Create odd_or_even.py
    You can check your number is odd or even by this lines of code 
    even is the type of number that can be devided by 2 and the remain by using % is 0 
    odd is the type of number that can't be devided by 2 and the remain by using % is not 0 either it's can be 1, 2, 3 and so on.

---
+++ odd_or_even.py
@@ -0,0 +1,7 @@
+number = int(input("Type the numbe that you wunna check: "))
+check = number % 2
+
+if check == 0:
+  print(f"{number} is even due to it can devided by 2 and the remain is int")
+else: 
+  print(f"{number} is odd due to it can't devide by 2 and the remian is float")  
