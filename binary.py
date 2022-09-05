5d10c76d481c9730dd25062e8001aa44f92ad402
Austin Rolert <austinrolert@gmail.com>
2022-09-05 10:54:21 +0630

    Create binary.py
    the program that turn Decimal to Binary

---
+++ binary.py
@@ -0,0 +1,10 @@
+def convertToBinary(n):
+   if n > 1:
+       convertToBinary(n//2)
+   print(n % 2,end = '')
+
+# decimal number
+dec = int(input("enter decimal number: "))
+
+convertToBinary(dec)
+print()
