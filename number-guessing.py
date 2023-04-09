69056dbe1384802d70b460df7d493e2602cb6468
Austin Rolert <austinrolert@gmail.com>
2023-04-10 04:10:52 +0630

    Create number-guessing.py
    Number Guessing Game
    
    This is a simple number guessing game written in Python that runs in the terminal. The game generates a random number between 1 and 100 and prompts the user to guess the number within a certain number of attempts. The number of attempts varies depending on the difficulty level selected by the user (easy or hard).
    
    Usage
    
    To start the game, navigate to the directory where you downloaded the game code and run the following command in the terminal:
    
    Copy code
    python number_guessing_game.py
    The game will then prompt you to select a difficulty level (easy or hard) and provide instructions on how to play.
    
    Code Explanation
    
    Importing Libraries
    python
    Copy code
    import random
    We import the random library in order to generate a random number for the user to guess.
    
    Setting Up the Game
    python
    Copy code
    attempts_easy = 10
    attempts_hard = 5
    We define two variables attempts_easy and attempts_hard to hold the number of attempts allowed for each difficulty level.
    
    Getting the User's Choice of Difficulty Level
    python
    Copy code
    while True:
        level = input("Choose a difficulty level. Type 'easy' or 'hard': ")
        if level == "easy":
            attempts = attempts_easy
            break
        elif level == "hard":
            attempts = attempts_hard
            break
        else:
            print("Error: Invalid input. Please try again.")
    We use a while loop to prompt the user to enter their choice of difficulty level. If the user enters "easy", we set the number of attempts to attempts_easy and exit the loop using the break statement. If the user enters "hard", we set the number of attempts to attempts_hard and exit the loop. If the user enters anything else, we print an error message and continue the loop until the user enters a valid input.
    
    Generating the Random Number
    python
    Copy code
    number = random.randint(1, 100)
    We use the randint() function from the random library to generate a random integer between 1 and 100. This is the number that the user will try to guess.
    
    Getting the User's Guess
    python
    Copy code
    for i in range(attempts):
        guess = int(input("Guess the number: "))
        if guess == number:
            print(f"Congratulations! You guessed the number {number} correctly!")
            quit()
        elif guess < number:
            print("Too low.")
        elif guess > number:
            print("Too high.")
        print(f"You have {attempts-i-1} attempts remaining.")
    We use a for loop to prompt the user to enter their guess a certain number of times (depending on the difficulty level). If the user correctly guesses the number, we print a congratulatory message using an f-string and exit the program using the quit() function. If the user's guess is too low, we print "Too low." If the user's guess is too high, we print "Too high." We also print the number of attempts remaining using an f-string.
    
    Conclusion
    This covers the major parts of the code for the number guessing game. Have fun playing the game!

---
+++ number-guessing.py
@@ -0,0 +1,54 @@
+import random
+
+print("""                     
+ __     __     ______     __         ______     ______     __    __     ______    
+/\ \  _ \ \   /\  ___\   /\ \       /\  ___\   /\  __ \   /\ "-./  \   /\  ___\   
+\ \ \/ ".\ \  \ \  __\   \ \ \____  \ \ \____  \ \ \/\ \  \ \ \-./\ \  \ \  __\   
+ \ \__/".~\_\  \ \_____\  \ \_____\  \ \_____\  \ \_____\  \ \_\ \ \_\  \ \_____\ 
+  \/_/   \/_/   \/_____/   \/_____/   \/_____/   \/_____/   \/_/  \/_/   \/_____/ 
+                                                                                  
+
+""")
+      
+print(f"Ok take a guess I'm think a number between 1 and 100")
+GUESS_NUMBER = random.randint(1,100+1)
+print(GUESS_NUMBER)
+
+while True:
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+
+    if difficulty == "easy":
+        attempts = 10
+        break
+    elif difficulty == "hard":
+        attempts = 5
+        break
+    else:
+        print("Invalid difficulty level. Please choose 'easy' or 'hard'.")
+
+
+while attempts > 0:
+    print(f"You have {attempts} attamps to guess the number")
+    guess = int(input("Enter your guess: "))
+    if guess == GUESS_NUMBER:
+        print(f"You win")
+        quit()
+    elif guess > GUESS_NUMBER:
+        if guess - GUESS_NUMBER > 10:  
+          print("Too high")
+        elif guess - GUESS_NUMBER < 5:
+            print("You are in close range 5")
+        else:
+            print("You are in clsoe range 10")
+          
+
+    elif guess < GUESS_NUMBER:
+        if GUESS_NUMBER - guess >10:
+          print("Too Low")
+        elif GUESS_NUMBER - guess < 5:
+            print("You are in close range 5")
+        else:
+            print("You are in close range 10")
+
+    attempts-=1
+print("Game over you run out of attamps")
